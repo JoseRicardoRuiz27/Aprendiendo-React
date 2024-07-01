32995fa1829e07515ae02378812895e40a554dd1
Jose Ruiz <jruiz793ric@gmail.com>
2024-07-01 17:45:03 -0300

    subir archivo

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css"
+
+export function App(){
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/midudev" />
+                <div>
+                    <strong>Don Ricardo Ruiz</strong>
+                    <span>@jruiz2712</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
